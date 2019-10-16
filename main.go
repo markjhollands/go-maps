139f815b5cbbeda9f4a918c333e181aac64c0657
Mark Hollands <markjhollands@gmail.com>
2019-10-16 19:33:31 +0100

    Multiple ways to make a map

--- main.go
+++ main.go
@@ -9,4 +9,22 @@ func main() {
 	}
 
 	fmt.Println(colours)
+
+	// Different way of declaring Maps
+	var colours1 map[string]string
+
+	fmt.Println(colours1)
+
+	// Another Different way of declaring Maps
+	colours2 := make(map[string]string)
+
+	colours2["white"] = "#ffffff"
+	colours2["blue"] = "#0000ff"
+
+	fmt.Println(colours2)
+	fmt.Println(colours2["white"])
+
+	delete(colours2, "blue")
+
+	fmt.Println(colours2)
 }
